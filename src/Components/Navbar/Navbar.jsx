6119ead33460e9c46e750326ce6bfa1bdd8a16e3
kaveesha nipun <kaveeshanipu0@gmail.com>
2025-04-08 00:04:59 +0530

    Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
     <nav>
-         <img src="" alt="" />
+         <img src={logo} alt="" />
+         <ul>
+            <li>Home</li>
+            <li>program</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li>Contact us </li>
+
+
+         </ul>
     </nav>
   )
 }
